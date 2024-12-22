f1fb9838628e8d2eeb6b0cad02af8bf2fc397e61
Axel Matstoms <axel.matstoms@gmail.com>
2024-12-22 07:00:56 +0100

    Add day 22

---
+++ 22/solve.py
@@ -0,0 +1,77 @@
+import collections
+
+
+def solve_p1(path):
+    with open(path) as f:
+        nums = [int(x) for x in f.read().splitlines()]
+
+    res = 0
+    for init in nums:
+        secret = init
+        for _ in range(2000):
+            secret ^= secret << 6
+            secret &= 0xffffff
+
+            secret ^= secret >> 5
+            secret &= 0xffffff
+
+            secret ^= secret << 11
+            secret &= 0xffffff
+        res += secret
+
+    return res
+
+
+def solve_p2(path):
+    with open(path) as f:
+        nums = [int(x) for x in f.read().splitlines()]
+
+    res = 0
+    scores = collections.Counter()
+    for init in nums:
+        secret = init
+        suffix = []
+        last = init % 10
+        buyer_score = collections.Counter()
+
+        for _ in range(2000):
+            secret ^= secret << 6
+            secret &= 0xffffff
+
+            secret ^= secret >> 5
+            secret &= 0xffffff
+
+            secret ^= secret << 11
+            secret &= 0xffffff
+            digit = secret % 10
+
+            delta = digit - last
+            last = digit
+
+            suffix.append(delta)
+            if len(suffix) > 4:
+                suffix.pop(0)
+            else:
+                continue
+
+            key = tuple(suffix)
+            if key not in buyer_score:
+                buyer_score[key] = digit
+
+        scores += buyer_score
+
+    res = max(scores.values())
+
+    return res
+
+
+def main():
+    assert solve_p1("example") == 37327623
+    print(solve_p1("input"))
+
+    assert solve_p2("example2") == 23
+    print(solve_p2("input"))
+
+
+if __name__ == "__main__":
+    main()
